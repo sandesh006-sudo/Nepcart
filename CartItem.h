e82a960cf21403d301156725828a13045c6e66c8
Sandesh <kattelsandesh99@gmail.com>
2026-07-28 06:43:14 +0545

    Create CartItem.h

---
+++ CartItem.h
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class cartitem
+{
+    private:
+    int productid;
+    int quantity;
+    public:
+    cartitem(int id=0,int qty=0)
+    {
+        productid=id;
+        quantity=qty;
+    }
+    int getproductid()
+    {
+        return productid;
+    }
+    int getproductquantity()
+    {
+        return quantity;
+    }
+};
