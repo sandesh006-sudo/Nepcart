9e0ab7c5d4fc2538a9a185201425a3cb23cdcc11
Sandesh <kattelsandesh99@gmail.com>
2026-08-04 18:52:30 +0545

    Create admin.h

---
+++ admin.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class admin
+{
+private:
+    string username;
+    string password;
+
+public:
+
+    admin()
+    {
+        username = "admin";
+        password = "1234";
+    }
+
+
+    bool login(string u, string p)
+    {
+        if(username == u && password == p)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
