ed00bfe9c54f0b810b83861284248fb718dd0e50
Sabal <sabalcs7@gmail.com>
2026-08-08 22:46:49 +0545

    Final Version of the Project

--- ProductManager.h
+++ ProductManager.h
@@ -1,41 +1,46 @@
 #pragma once
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<sstream>
-#include<cctype>
-#include <filesystem>
-#include<vector>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include "Product.h"
+#include "utilities.h"
 
 using namespace std;
 
-class ProductManager{
+class ProductManager
+{
+private:
     const string PRODUCT_FILE;
-    // ProductManager has-a Product
+
     vector<Product> products;
-    public:
-    ProductManager():PRODUCT_FILE("products.txt"){
+
+public:
+    ProductManager() : PRODUCT_FILE("products.txt")
+    {
         loadProducts();
     }
 
-    // to lowercase a string
-    string lower(string str){
-        for(char& c : str){
-            c = tolower(c);
-        }
-        return str;
-    }
+    // ================= LOAD PRODUCTS =================
 
     void loadProducts()
     {
         ifstream fin(PRODUCT_FILE);
-        if(!fin.is_open()){
+
+        if (!fin.is_open())
             return;
-        }
+
         string line;
-        while(getline(fin, line)){
+
+        while (getline(fin, line))
+        {
+            if (line.empty())
+                continue;
+
             stringstream ss(line);
+
             string s_id;
             string s_name;
             string s_category;
@@ -53,172 +58,303 @@ class ProductManager{
                 s_name,
                 s_category,
                 stod(s_price),
-                stoi(s_quantity)
-            );
+                stoi(s_quantity));
+
             products.push_back(p);
         }
+
         fin.close();
     }
 
-    void addProduct(){
+    // ================= ADD PRODUCT =================
+
+    void addProduct()
+    {
         int id;
         string name;
         string category;
         double price;
         int quantity;
-        while(true){
+
+        while (true)
+        {
             cout << "Product ID: ";
-            cin >> id;
-            if(!idExists(id))
+            id = getInt();
+
+            if (!idExists(id))
                 break;
-            cout << "Product with that ID already exists! Enter ID again!\n";
+
+            cout << "Product with that ID already exists!"
+                 << " Enter ID again!\n";
         }
-        cout<<"Product name: ";getline(cin >> ws, name);
-        cout<<"Product category: ";getline(cin >> ws, category);
-        cout<<"Product price: ";cin>>price;
-        cout<<"Product quantity: ";cin>>quantity;
-        Product p(id,name,category,price,quantity);
+
+        cout << "Product name: ";
+        getline(cin >> ws, name);
+
+        cout << "Product category: ";
+        getline(cin >> ws, category);
+
+        cout << "Product price: ";
+        cin >> price;
+
+        cout << "Product quantity: ";
+        quantity = getInt();
+
+        Product p(
+            id,
+            name,
+            category,
+            price,
+            quantity);
+
         products.push_back(p);
+
         saveProducts();
-        cout<<"Product added successfully!\n";
+
+        cout << "Product added successfully!\n";
     }
 
-    void deleteProduct(){
-        int id;
-        cout << "Enter Product ID to delete: ";
-        cin >> id;
+    // ================= DELETE PRODUCT =================
 
-        for(int i = 0; i < products.size(); i++){
-            if(products[i].getId() == id){
+    void deleteProduct()
+    {
+        int id;
+
+        cout << "Enter Product ID to delete: ";
+        id = getInt();
+
+        for (int i = 0; i < products.size(); i++)
+        {
+            if (products[i].getId() == id)
+            {
                 products.erase(products.begin() + i);
+
                 saveProducts();
 
-                cout << "Product with ID " << id << " deleted successfully!\n";
+                cout << "Product with ID "
+                     << id
+                     << " deleted successfully!\n";
+
                 return;
             }
         }
-        cout << "Product with ID " << id << " doesn't exist!\n";
+
+        cout << "Product with ID "
+             << id
+             << " doesn't exist!\n";
     }
 
+    // ================= UPDATE PRODUCT =================
 
-    void updateProduct(){
+    void updateProduct()
+    {
         int id;
-        cout<<"Enter ID of product to update: ";
-        cin>>id;
+
+        cout << "Enter ID of product to update: ";
+        id = getInt();
+
         int i;
-        for(i=0;i<products.size();i++){
-            if(id==products[i].getId())
+
+        for (i = 0; i < products.size(); i++)
+        {
+            if (id == products[i].getId())
                 break;
         }
-        if(i==products.size()){
-            cout<<"Product with ID "<<id<<" doesn't exist!\n";
+
+        if (i == products.size())
+        {
+            cout << "Product with ID "
+                 << id
+                 << " doesn't exist!\n";
+
             return;
         }
+
         string name;
         string category;
         double price;
         int quantity;
         int choice;
-        while(true){
-            cout<<"WHAT TO CHANGE?\n";
-            cout<<"1. Name\n";
-            cout<<"2. Category\n";
-            cout<<"3. Price\n";
-            cout<<"4. Quantity\n";
-            cin>>choice;
-            if(choice>=1 && choice<=4)
+
+        while (true)
+        {
+            cout << "\nWHAT TO CHANGE?\n";
+            cout << "1. Name\n";
+            cout << "2. Category\n";
+            cout << "3. Price\n";
+            cout << "4. Quantity\n";
+
+            choice = getInt();
+
+            if (choice >= 1 && choice <= 4)
                 break;
+
+            cout << "Invalid choice!\n";
         }
-        switch(choice){
-            case 1:
-                cout<<"Product name: ";
-                getline(cin >> ws, name);
-                products[i].setName(name);
-                break;
-            case 2:
-                cout<<"Product category: ";
-                getline(cin >> ws, category);
-                products[i].setCategory(category);
-                break;
-            case 3:
-                cout<<"Product price: ";
-                cin>>price;
-                products[i].setPrice(price);
-                break;  
-            case 4:
-                cout<<"Product quantity: ";
-                cin>>quantity;
-                products[i].setQuantity(quantity);
-                break;
+
+        switch (choice)
+        {
+        case 1:
+
+            cout << "Product name: ";
+            getline(cin >> ws, name);
+
+            products[i].setName(name);
+
+            break;
+
+        case 2:
+
+            cout << "Product category: ";
+            getline(cin >> ws, category);
+
+            products[i].setCategory(category);
+
+            break;
+
+        case 3:
+
+            cout << "Product price: ";
+            cin >> price;
+
+            products[i].setPrice(price);
+
+            break;
+
+        case 4:
+
+            cout << "Product quantity: ";
+            quantity = getInt();
+
+            products[i].setQuantity(quantity);
+
+            break;
         }
+
         saveProducts();
     }
 
-    void searchByID(){
-        int id;
-        cout << "Enter Product ID: ";
-        cin >> id;
+    // ================= FIND PRODUCT =================
 
-        for(const Product& p : products){
-            if(p.getId() == id){
-                cout << "\nProduct found!\n";
-                p.display();
-                return;
-            }
+    Product *findByID(int id)
+    {
+        for (Product &p : products)
+        {
+            if (p.getId() == id)
+                return &p;
         }
-        cout << "Product with ID " << id<< " doesn't exist!\n";
+
+        return nullptr;
     }
 
-    void searchByName(){
+    // ================= SEARCH BY ID =================
+
+    void searchByID()
+    {
+        int id;
+
+        cout << "Enter Product ID: ";
+        id = getInt();
+
+        Product *product = findByID(id);
+
+        if (product != nullptr)
+        {
+            cout << "\nProduct found!\n";
+
+            product->display();
+
+            return;
+        }
+
+        cout << "Product with ID "
+             << id
+             << " doesn't exist!\n";
+    }
+
+    // ================= SEARCH BY NAME =================
+
+    void searchByName()
+    {
         string name;
+
         cout << "Enter Product name: ";
         getline(cin >> ws, name);
 
-        for(const Product& p : products)
+        for (const Product &p : products)
         {
-            if(lower(p.getName())== lower(name)){
+            if (lower(p.getName()) == lower(name))
+            {
                 cout << "\nProduct found!\n";
+
                 p.display();
+
                 return;
             }
         }
-        cout << "Product with name " << name << " doesn't exist!\n";
+
+        cout << "Product with name "
+             << name
+             << " doesn't exist!\n";
     }
 
-    void displayAll(){
-        if(products.empty()){
+    // ================= DISPLAY ALL =================
+
+    void displayAll()
+    {
+        if (products.empty())
+        {
             cout << "No products available.\n";
             return;
         }
 
         cout << "\n===== ALL PRODUCTS =====\n";
-        for(const Product& p : products){
+
+        for (const Product &p : products)
+        {
             p.display();
+
             cout << "------------------------\n";
         }
     }
 
-    void saveProducts(){
-        ofstream f(PRODUCT_FILE,ios::trunc);
-        if(!f.is_open()){
+    // ================= SAVE PRODUCTS =================
+
+    void saveProducts()
+    {
+        ofstream f(
+            PRODUCT_FILE,
+            ios::trunc);
+
+        if (!f.is_open())
+        {
             cout << "Cannot open products file.\n";
             return;
         }
-        for(const Product& p : products){
+
+        for (const Product &p : products)
+        {
             f << p.getId() << '|'
-                << p.getName() << '|'
-                << p.getCategory() << '|'
-                << p.getPrice() << '|'
-                << p.getQuantity() << '\n';
+              << p.getName() << '|'
+              << p.getCategory() << '|'
+              << p.getPrice() << '|'
+              << p.getQuantity()
+              << '\n';
         }
+
         f.close();
     }
 
-    bool idExists(int id){
-        for(const Product& p : products){
-            if(p.getId() == id)
+    // ================= CHECK ID =================
+
+    bool idExists(int id)
+    {
+        for (const Product &p : products)
+        {
+            if (p.getId() == id)
                 return true;
         }
+
         return false;
     }
 };
